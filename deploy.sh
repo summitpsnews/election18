36c9d17b376a0d557a4950d2532fafbdd233802b
Justin Butera <jbutera94@gmail.com>
2018-12-07 21:16:53 +0000

    Remove push_to_hostgator script and add new deploy script using Zeit Now

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git stash
+jekyll build
+now ./_site
+now alias -A ../now.json
+git stash pop
--- push_to_hostgator.sh
+++
@@ -1,2 +0,0 @@
-ssh summitpsnews@summitpsnewsmagazine.com "rm -rf ~/public_html/summitpsnews"
-scp -rp _site summitpsnews@summitpsnewsmagazine.com:~/public_html/summitpsnews
