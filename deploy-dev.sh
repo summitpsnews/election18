16eb8accb9200c8fb21c53e45888a3f510012289
Justin Butera <jbutera94@gmail.com>
2018-12-10 17:48:29 +0000

    Add line to deploy script to remove unused instances

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,3 +1,4 @@
 jekyll build
 now ./_site -A now-dev.json
 now alias -A now-dev.json
+now rm dev-summitpsnewsmagazine --safe --yes
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ git stash
 jekyll build
 now ./_site -A now-prod.json
 now alias -A now-prod.json
+now rm summitpsnewsmagazine --safe --yes
 git stash pop
