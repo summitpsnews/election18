dcafe712cb50c13f7aa3484ff805a5811b071236
Justin Butera <jbutera94@gmail.com>
2018-12-13 16:45:49 +0000

    Improve regenerate author pages script

--- regenerate-author-pages.sh
+++ regenerate-author-pages.sh
@@ -2,6 +2,5 @@ rm authors/*
 grep -Pazohr "^(?<=authors:\n)(- .*?\n)+" _posts --exclude=2018-09-27-markdown-demo.md | sort | uniq | sed '/^$/d; s/- //g' | while read name
 do
     slug=$(echo "$name" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
-    cp author-page-template.html authors/$slug.html
-    sed -i -e "s/AUTHOR NAME/$name/" authors/$slug.html
+    sed -e "s/AUTHOR NAME/$name/" author-page-template.html > authors/$slug.html
 done
