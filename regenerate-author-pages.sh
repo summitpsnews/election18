dce7aa72a2b3380ce81d61caf0343b32c9b6bca2
Summit PS News Magazine <edeornellas@summitps.org>
2019-05-20 16:32:15 +0000

    Modify author page regeneration script to include ALL articles, regardless of location

--- regenerate-author-pages.sh
+++ regenerate-author-pages.sh
@@ -1,5 +1,5 @@
 rm authors/*
-grep -Pazohr "^(?<=authors:\n)(- .*?\n)+" _posts --exclude=2018-09-27-markdown-demo.md | sort | uniq | sed '/^$/d; s/- //g' | while read name
+grep -Pazohr "^(?<=authors:\n)(- .*?\n)+" . --exclude=_site --exclude=2018-09-27-markdown-demo.md | sort | uniq | sed '/^$/d; s/- //g' | while read name
 do
     slug=$(echo "$name" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
     sed -e "s/AUTHOR NAME/$name/" author-page-template.html > authors/$slug.html
